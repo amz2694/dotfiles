eb1edb163114b060010efc414d10f60aea2a8f0f
amz2694 <alimiree.zade@gmail.com>
2023-02-06 20:58:26 +0330

    Dotfiles

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
+
+echo "Polybar launched..."
